ebb89670c0155d39659156794c4829c2ec58e03b
Ubuntu <ubuntu@ip-172-31-37-14.ec2.internal>
2019-12-16 16:48:58 +0000

    create template questions.html

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'faq_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...merry christmas!'
+@app.route('/get_questions')
+def get_questions():
+    return render_template("questions.html", questions=mongo.db.questions.find())
 
 
 if __name__ == '__main__':
