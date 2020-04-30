fcaa829c871f824997918f61363a7eb59986e712
Petra  Mellbrand <47560139+ringarochkryss@users.noreply.github.com>
2020-04-30 23:00:43 +0200

    add port and ip numbers
    To deploy on Heroku

--- app.py
+++ app.py
@@ -65,7 +65,6 @@ def delete_question(question_id):
     return redirect(url_for('get_questions'))
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
